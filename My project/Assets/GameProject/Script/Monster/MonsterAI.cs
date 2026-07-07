36d85e132cdd6f3e37a68450c25348fde5f5d0d6
rodollp <zeus02021101@gmail.com>
2026-07-07 13:29:53 +0900

    Update MonsterAI.cs

--- My project/Assets/GameProject/Script/Monster/MonsterAI.cs
+++ My project/Assets/GameProject/Script/Monster/MonsterAI.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterAI : MonoBehaviour
 {
-   
+    [SerializeField] private MonsterStatus status;
+    [SerializeField] Transform target;
+    private NavMeshAgent agent;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();   
+        agent.speed = status.MoveSpeed;
+
+        FindPlayer();
+    }
+
+    void FindPlayer()
+    {
+        if (target != null) return;
+
+        PlayerStatus playerStatus = FindAnyObjectByType<PlayerStatus>();
+
+        if (playerStatus != null)
+        {
+            target = playerStatus.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if( agent ==  null ) return;
+        agent.SetDestination(target.position);
+    }
+
 }

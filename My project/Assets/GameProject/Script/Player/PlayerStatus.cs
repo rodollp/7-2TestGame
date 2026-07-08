1e75ecb707c2681710fbef69b8cdc03ffe9aa1b0
rodollp <zeus02021101@gmail.com>
2026-07-08 12:38:26 +0900

    r

--- My project/Assets/GameProject/Script/Player/PlayerStatus.cs
+++ My project/Assets/GameProject/Script/Player/PlayerStatus.cs
@@ -4,24 +4,57 @@ public class PlayerStatus : CreatureStatus
 {
     [SerializeField] private PlayerData data;
 
-    public string Name {  get; private set; }
+    public string Name { get; private set; }
     public int Level { get; private set; }
-    public int CurrentExp {  get; private set; }
-    public int NeedExp {  get; private set; }
+    public int CurrentExp { get; private set; }
+    public int NeedExp { get; private set; }
 
     public float JumpPower { get; private set; }
 
     private void Awake()
+    {
+        StartStatus();
+    }
+
+    private void StartStatus()
     {
         Name = data.ThisName;
         MaxHp = data.MaxHp;
         AttackPower = data.AttackPower;
         MoveSpeed = data.MoveSpeed;
         JumpPower = data.JumpPower;
+        Level = data.StartLevel;
+        NeedExp = data.NeedExp;
+        CurrentExp = 0;
 
         CurrentHp = MaxHp;
     }
 
+
+    public void AddExp(int exp)
+    {
+        CurrentExp += exp;
+        LevelUp();
+    }
+    private void LevelUp()
+    {
+        while (CurrentExp >= NeedExp)
+        {
+            Level += 1;
+            CurrentExp -= NeedExp;
+
+            ApplyLevelUpStats();
+        }
+
+    }
+
+    private void ApplyLevelUpStats()
+    {
+        MaxHp += 5;
+        CurrentHp += 5;
+        AttackPower += 5;
+        NeedExp += 10;
+    }
     public override void TakeDamage(int damage)
     {
         if (IsDead) return;
